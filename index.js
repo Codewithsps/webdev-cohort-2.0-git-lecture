f3dc58cb11166d8681fc30542bf469f3445b6f0e
codewithsps <codewithsps>
2026-02-10 03:38:15 +0530

    i have just merged changes from feat/a branch

--- index.js
+++ index.js
@@ -1,5 +1,15 @@
 function add(a, b) {
     return a + b;
 }
+
+function subtract(a, b) {
+    if(!(a > b)) throw new Error("First number must be greater than second number");
+    return a - b;
+}
+
 add(2, 3); // returns 5
 add (10, 20); // returns 30
+add(0, 0); // returns 0
+add(-5, 5); // returns 0
+
+subtract(5, 2); // returns 3
